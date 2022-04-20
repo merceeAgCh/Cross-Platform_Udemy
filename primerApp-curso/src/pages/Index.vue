a950c089d3d1f6df9823cb473c178bc2eec5c650
Mercedes AC <breakh_070795@hotmail.com>
2022-04-19 19:10:53 -0500

    Events -Keyboard and more, section 3 class number #16

--- primerApp-curso/src/pages/Index.vue
+++ primerApp-curso/src/pages/Index.vue
@@ -1,17 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <input
+      v-model="message"
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"
+      @mouseenter="alertMessage"
+      @mouseleave="clearMessage"
+    />
+    <button @click="message = ''">Clear</button>
+    <h5>{{ message }}</h5>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  data() {
+    return {
+      message: "Un mensaje para Vue JS",
+    };
+  },
+  methods: {
+    clearMessage() {
+      this.message = "";
+    },
+    /* handleKeyup(e) {
+      console.log(e);
+      if (e.keyCode == 27) {
+        this.clearMessage();
+      } else if (e.keyCode == 13) {
+        this.alertMessage();
+      }
+    }, */
+    alertMessage() {
+      alert(this.message);
+    },
+  },
+});
 </script>
+<style></style>
